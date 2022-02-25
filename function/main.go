c81cc679a8e594df9c850dcf4bf95a0b36428806
titikkoma88 <jie.koma88@gmail.com>
2022-02-25 17:24:14 +0700

    function continuation

--- function/main.go
+++ function/main.go
@@ -3,30 +3,12 @@ package main
 import "fmt"
 
 func main()  {
-	printMyResult()
-	fmt.Println("==========")
-	printMyResult2("Saya sedang")
-	printMyResult2("belajar")
-	printMyResult2("Golang Fundamental")
-	fmt.Println("==========")
-	sentence2 := printMyResult3("Materi")
-	fmt.Println(sentence2)
-
+	result := add(10, 20)
+	fmt.Println(result)
 }
 
-// 1. Input
-// 2. Proses
-// 3. Output
-
-func printMyResult()  {
-	fmt.Println("Saya sedang belajar golang")
+func add(number, numberTwo int) int {
+	return number + numberTwo
+	
 }
 
-func printMyResult2(sentence string)  {
-	fmt.Println(sentence)
-}
-
-func printMyResult3(sentence2 string) string {
-	newSentence := sentence2 + " function"
-	return newSentence
-}
