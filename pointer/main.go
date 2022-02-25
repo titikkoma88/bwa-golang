25e0f14891ec431ceebedf861c45f533dd20022d
titikkoma88 <jie.koma88@gmail.com>
2022-02-26 01:09:09 +0700

    pointer case, change value on function

--- pointer/main.go
+++ pointer/main.go
@@ -15,17 +15,42 @@ func main()  {
 	// fmt.Println(*numberB)
 	// fmt.Println(numberA)
 
-	var numberA int = 5
-	var numberB *int = &numberA
+	// var numberA int = 5
+	// var numberB *int = &numberA
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
 
-	numberA = 20
+	// numberA = 20
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
 
+	number := 5
+	fmt.Println("Alamat memory :", &number)
+	fmt.Println("Nilai awal :", number)
+
+	change(number, 100)
+	fmt.Println("Nilai akhir :", number)
+	fmt.Println("Alamat memory :", &number)
+
+	fmt.Println("=========")
+
+	change2(&number, 100)
+	fmt.Println("Nilai akhir 2 :", number)
+	fmt.Println("Alamat memory 2 :", &number)
+}
+
+func change(old int, new int)  {
+	old = new
+	fmt.Println("Alamat memory :", &old)
+	fmt.Println("Didalam function :", old)
+}
+
+func change2(old *int, new int)  {
+	*old = new
+	fmt.Println("Alamat memory 2 :", old)
+	fmt.Println("Didalam function 2 :", *old)
 }
