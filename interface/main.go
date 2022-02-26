5b7acb927a0c3c3683f633bf81de56e6ef7a8925
titikkoma88 <jie.koma88@gmail.com>
2022-02-26 13:26:31 +0700

    problem without interface

--- interface/main.go
+++ interface/main.go
@@ -1,9 +1,40 @@
 package main
 
-type Luas interface {
+import "fmt"
 
+type Luas interface {
+	HitungLuas() int
 }
 
-func main()  {
-	
+type Persegi struct {
+	Sisi int
+}
+
+func (persegi Persegi) HitungLuas() int {
+	return persegi.Sisi * persegi.Sisi
+}
+
+type PersegiPanjang struct {
+	Panjang int
+	Lebar   int
+}
+
+func (persegiPanjang PersegiPanjang) HitungLuas() int {
+	return persegiPanjang.Panjang * persegiPanjang.Lebar
+}
+
+func main() {
+	// bangunDatar := Persegi{Sisi: 4}
+	bangunDatar := PersegiPanjang{Panjang: 6, Lebar: 5}
+
+	luas := SeberapaLuas(bangunDatar)
+	fmt.Println("Luas : ", luas)
+}
+
+func SeberapaLuas(bangunDatar PersegiPanjang) int {
+	return bangunDatar.HitungLuas()
+}
+
+func SeberapaLuas2(bangunDatar Persegi) int {
+	return bangunDatar.HitungLuas()
 }
